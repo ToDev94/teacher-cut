ab7d700fe763875ac7fc925c13f410b086c32dab
ToDev94 <nefoucitoufik@gmail.com>
2023-04-29 00:04:18 +0100

    fixing the formidable import statment

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse, PageConfig } from "next";
-import formidable from "formidable";
+const formidable = require("formidable");
 import fs from "fs";
 const fsp = require("fs").promises;
 import path from "path";
